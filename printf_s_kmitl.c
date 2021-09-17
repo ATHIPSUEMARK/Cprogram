0d889892dd13c5bbdfb10d8b0c2e937b7f009984
ATHIP SUEMARK <88649528+ATHIPSUEMARK@users.noreply.github.com>
2021-09-17 18:09:42 +0700

    Add files via upload

---
+++ printf_s_kmitl.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+
+    printf("%-40s%40s" ,"Bangkok","Thailand");
+
+    printf("%1840s" ,"");
+
+    printf("%-40s" ,"King Mongkut's Institute of Technology Ladkrabang");
+    
+    
+return 0;
+
+}
